814e534a2c1febf299ecfef8ee2b8e86cc887f75
Laima <pileliene.laima@gmail.com>
2023-11-17 21:59:38 +0200

    number game

--- src/components/namuDarbai/Skaiciuoklis.jsx
+++ src/components/namuDarbai/Skaiciuoklis.jsx
@@ -15,50 +15,69 @@ export function Skaiciuoklis() {
         width: '50px',
         cursor: 'pointer',
     };
+    const naudojamasZymuo = {
+        color: 'green',
 
-    const arr = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+    }
+
+    const arr = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
     const arr2 = ["+", "-", "/", "*"];
+    const arr3 = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+
 
 
     let [nr, setNr] = useState(0);
     let [zenklas, setZenklas] = useState("+");
+    let [nr2, setNr2] = useState(0);
 
 
-    function updateValue(num) {
-        console.log("test")
-        setNr(num)
+    function updateValue(asdf) {
+        setNr(asdf)
+    }
+    function updateZenklas(zen) {
+        setZenklas(zen)
+    }
+    function updateValue3(y) {
+        setNr2(y)
     }
 
-    
+    function answer(val1, val2, val3) {
 
-    function CreateMyButton(number) {
-        return (
-            <button style={numberStyle} onClick={() => updateValue(number)}>{number}</button>
-        )
+        switch (val2) {
+            case "+":
+                return val1 + val3;
+            case "-":
+                return val1 - val3;
+            case "*":
+                return val1 * val3;
+            case "/":
+                return val1 / val3;
+            default: return 0
+        }
+
     }
-
 
     return (
-        <div>
-            <div className={style.numberContainer}>
 
-                <div className={style.gridContainer}>
-                    {arr.map((num) => CreateMyButton(num))}
-                </div>
+        <div className={style.numberContainer}>
 
-                <div className={style.gridContainerMath}>
-                    {arr2.map((num) => CreateMyButton(num))}
-                </div>
-
-                <div className={style.gridContainer}>
-                    {arr.map((num) => CreateMyButton(num))}
-                </div>
+            <div className={style.gridContainer}>
+                {arr.map((arrItem) => <button style={numberStyle} onClick={() => updateValue(arrItem)} >{arrItem}</button>)}
+                <span className={style.naudojamasZymuo}>{nr}</span> 
             </div>
-            <p>Atsakymas: {nr} </p>
+            <div className={style.gridContainerMath}>
+                {arr2.map((arr2Item) => <button style={numberStyle} onClick={() => updateZenklas(arr2Item)} >{arr2Item}</button>)}
+                <span className={style.naudojamasZymuo}>{zenklas}</span>
+            </div>
+
+            <div className={style.gridContainer}>
+                {arr3.map((arr3Item) => <button style={numberStyle} onClick={() => updateValue3(arr3Item)} >{arr3Item}</button>)}
+                {nr2}
+            </div>
+
+            <p>Atsakymas: {answer(nr, zenklas, nr2)} </p>
         </div>
 
     );
 
 }
-
-
